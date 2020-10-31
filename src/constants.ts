bb9a1e28ae509979aaf3b22f55f4bb52f2d469a0
pfftdammitchris <pfftdammitchris@gmail.com>
2020-10-31 05:57:47 -0700

    G

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,12 @@
+export const eventId = {
+	RETRIEVED_ROOT_CONFIG: 'retrieved.root.config',
+	RETRIEVED_APP_CONFIG: 'retrieved.app.config',
+	RETRIEVED_VERSION: 'retrieved.root.config.version',
+	RETRIEVED_ROOT_BASE_URL: 'retrieved.root.config.base.url',
+	RETRIEVED_APP_ENDPOINT: 'retrieved.app.endpoint',
+	RETRIEVED_APP_BASE_URL: 'retrieved.app.base.url',
+}
+
 export const RETRIEVE_NOODL_OBJECTS_JSON = 'RETRIEVE_NOODL_OBJECTS_JSON'
 export const RETRIEVE_NOODL_OBJECTS_YML = 'RETRIEVE_NOODL_OBJECTS_YML'
 export const RETRIEVE_NOODL_PROPERTIES = 'RETRIEVE_NOODL_PROPERTIES'
