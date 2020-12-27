fad5e9626719f1aa03fb6200f66486f51be2a69a
Christopher Tran <pfftdammitchris@gmail.com>
2020-12-26 21:50:26 -0800

    G

--- dev/dev.ts
+++ dev/dev.ts
@@ -2,6 +2,7 @@ import axios from 'axios'
 import chalk from 'chalk'
 import fs from 'fs-extra'
 import path from 'path'
+import globby from 'globby'
 import { getFilePath } from '../src/utils/common'
 import createAggregator from '../src/api/createAggregator'
 
@@ -25,9 +26,18 @@ async function forEachDeepFile(
 	}
 }
 
+async function getFilesDeep(opts: { dir: string }) {
+	try {
+	} catch (error) {
+		console.error(`[${chalk.red(error.name)}]: ${error.message}`)
+	}
+}
+
 let count = 0
-forEachDeepFile('data/objects', (file) => {
-	console.log(file)
-	count++
-})
-console.log(count)
+// forEachDeepFile('data/objects', (file) => {
+// 	console.log(file)
+// 	count++
+// })
+// console.log(count)
+
+globby(['.png']).then(console.log).catch(console.error)
