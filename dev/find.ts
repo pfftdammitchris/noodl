1634d13a4173525dbb7b3b011d7821e5a5646980
pfftdammitchris <pfftdammitchris@gmail.com>
2021-01-04 22:04:21 -0800

    G

--- dev/find.ts
+++ dev/find.ts
@@ -1,3 +1,4 @@
+console.clear()
 import { config } from 'dotenv'
 config()
 import axios from 'axios'
@@ -5,82 +6,107 @@ import produce, { applyPatches, enablePatches } from 'immer'
 import chalk from 'chalk'
 import fs from 'fs-extra'
 import path from 'path'
-import { ActionType, ComponentType, identify } from 'noodl-types'
 import globby from 'globby'
 import isPlainObject from 'lodash/isPlainObject'
-import { forEachDeepKeyValue, getFilePath } from '../src/utils/common'
 import createAggregator from '../src/api/createAggregator'
+import { ActionType, ComponentType, identify } from 'noodl-types'
+import {
+	forEachDeepKeyValue,
+	getFilePath,
+	sortObjPropsByKeys,
+} from '../src/utils/common'
 import { AnyFn } from '../src/types'
 import { getLogoutPageObject } from './helpers'
 import * as T from './types'
 enablePatches()
 
-const format = (function () {
-	function basic(...fns) {
-		return function basicFormat(data) {
-			return fns.reduceRight((acc, fn) => acc(fn(data)), [])
-		}
-	}
-
-	const o = {
-		list(type: 'basic') {
-			switch (type) {
-				case 'basic':
-					return basic
-			}
-		},
-	}
-	return o
-})()
-
 const Find = (function () {
-	let transformers = []
-	let mods = []
-	let formats = []
-
 	const o = {
-		forEachDeepKeyValue<O = any>(
-			cb: (key: string, value: any, obj: T.PlainObject) => void,
-			obj: O | O[],
-		) {
-			if (Array.isArray(obj)) {
-				obj.forEach((v) => o.forEachDeepKeyValue(cb, v))
-			} else if (isPlainObject(obj)) {
-				Object.entries(obj).forEach(([key, value]) => {
-					cb(key, value, obj)
-					if (isPlainObject(value) || Array.isArray(value)) {
-						o.forEachDeepKeyValue(cb, value)
-					}
-				})
-			} else {
+		objectsThatContainKeys({
+			name = '',
+			keys,
+			objs,
+		}: {
+			name?: string
+			keys: string | string[]
+			objs: any
+		}) {
+			const keywords = Array.isArray(keys) ? keys : [keys]
+			const results = {
+				name,
+				keywords,
+				results: keywords.reduce(
+					(acc, keyword) => Object.assign(acc, { [keyword]: [] }),
+					{},
+				),
 			}
+			forEachDeepKeyValue(
+				(key, value, obj) =>
+					keywords.includes(key) && results.results[key].push(obj),
+				objs,
+			)
+			return results
+		},
+		getKeyCounts({
+			keys,
+			objs,
+		}: {
+			objs: any
+			keys?: string | string[]
+		}): { [key: string]: number } {
+			const keywords = Array.isArray(keys) ? keys : (keys && [keys]) || []
+			const results = {} as { [key: string]: number }
+			forEachDeepKeyValue((key, value, obj) => {
+				if (keywords.length) {
+					if (keywords.includes(key)) {
+						if (typeof results[key] !== 'number') results[key] = 0
+						results[key]++
+					}
+				} else {
+					if (typeof results[key] !== 'number') results[key] = 0
+					results[key]++
+				}
+			}, objs)
+			return sortObjPropsByKeys(results)
+		},
+		getPathnames({ name = '', objs }: { name?: string; objs: any }) {
+			const results = {
+				name,
+				results: [],
+			}
+			forEachDeepKeyValue((key, value, obj) => {
+				if (typeof value === 'string') {
+					if (value.startsWith('http')) {
+						!results.results.includes(value) && results.results.push(value)
+					} else if (/(path|resource)/i.test(key)) {
+						!results.results.includes(value) && results.results.push(value)
+					}
+				}
+			}, objs)
+			return results.results.sort()
 		},
 	}
+
 	return o
 })()
 
-const perf = (fn: Function) => {
-	console.time('perfing')
-	fn()
-	console.timeEnd('perfing')
-}
-const results = []
-
-perf(() => {
-	Find.forEachDeepKeyValue((key) => {
-		console.log(key)
-		results.push(key)
-	}, getLogoutPageObject())
-})
-
-// perf(() => {
-// 	forEachDeepKeyValue((key) => {
-// 		console.log(key)
-// 		results.push(key)
-// 	}, getLogoutPageObject())
+// const objsThatContainKeys = Find.objectsThatContainKeys({
+// 	name: 'Logout',
+// 	keys: 'path',
+// 	objs: getLogoutPageObject(),
 // })
 
-// fs.writeJsonSync(getFilePath('./hehe.json'), results, { spaces: 2 })
+// const keyCounts = Find.getKeyCounts({
+// 	objs: [],
+// })
+
+const getPathnames = Find.getPathnames({
+	objs: getLogoutPageObject(),
+})
+
+console.log(getPathnames)
+
+fs.writeJsonSync(getFilePath('./hehe.json'), getPathnames, { spaces: 2 })
 // globby(getFilePath('data/generated/json/**/*.json')).then(async (filepaths) => {
 // 	const objects = await Promise.all(
 // 		filepaths.map((filepath) => fs.readJsonSync(filepath)),
