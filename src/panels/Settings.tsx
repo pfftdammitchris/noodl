7d05da22b749ddb610997ed65cd1c64928af0bf1
Christopher Tran <pfftdammitchris@gmail.com>
2021-02-01 10:06:17 -0800

    G

--- src/panels/Settings.tsx
+++ src/panels/Settings.tsx
@@ -2,16 +2,17 @@ import React from 'react'
 import useCtx from '../useCtx'
 import { CLIConfigObject } from '../types'
 import cliConfig from '../cliConfig'
+import { getCliConfig } from '../utils/common'
 
 export interface SettingsProps {
 	defaults?: Partial<CLIConfigObject>
 }
 
 function Settings({ defaults }: SettingsProps) {
-	const { getConsumerConfig, setCaption } = useCtx()
+	const { setCaption } = useCtx()
 
 	React.useEffect(() => {
-		const consumerConfig = getConsumerConfig()
+		const consumerConfig = getCliConfig() as CLIConfigObject
 		cliConfig.merge({ ...defaults, ...consumerConfig })
 		setCaption('\nInitialized\n')
 	}, [])
