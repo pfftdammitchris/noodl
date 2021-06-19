7c4d6952c8a5c37a4351cb1c094daff1860b8283
Christopher Tran <pfftdammitchris@gmail.com>
2021-06-19 07:52:28 -0700

    Update(s) to lib

--- packages/noodl-common/src/stringifyDoc.ts
+++ packages/noodl-common/src/stringifyDoc.ts
@@ -1,18 +1,24 @@
-import { Document, parseDocument } from 'yaml'
+import { Document, parse, ToStringOptions, stringify } from 'yaml'
 
 /**
  * Returns the stringified output of the yaml document. If there are errors, it
- * returns a JSON stringified list of errors instead
+ * returns a stringified yaml output of the errors instead
  * @param { Document } doc
  */
-function stringifyDoc(doc: Document | undefined | null) {
+function stringifyDoc(
+	doc: Document | undefined | null,
+	opts?: ToStringOptions,
+) {
 	let result = ''
 
 	if (doc) {
 		if (doc.errors.length) {
-			result = JSON.stringify(doc.errors, null, 2)
+			result = stringify(doc.errors)
 		} else {
-			result = doc.toString()
+			result = doc.toString({
+				singleQuote: true,
+				...opts,
+			})
 		}
 	}
 
