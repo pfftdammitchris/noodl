4560c83681c4b2df54b5e6609e89cba34d5c938a
Christopher Tran <pfftdammitchris@gmail.com>
2021-06-24 08:49:19 -0700

    G

--- dev/refs.js
+++ dev/refs.js
@@ -1,4 +1,5 @@
 process.stdout.write('\x1Bc')
+const invariant = require('invariant')
 const u = require('@jsmanifest/utils')
 const yaml = require('yaml')
 const ntil = require('noodl-utils')
@@ -50,8 +51,16 @@ const parse = new ntil.Parser()
 const refs = new yaml.Document()
 const docs = ncom.loadFiles(
 	ncom.normalizePath(__dirname, '../generated/meet4d'),
-	{ as: 'map', type: 'doc' },
+	{
+		as: 'map',
+		type: 'doc',
+		spread: 'BaseDataModel',
+	},
 )
+
+console.log(docs.get('Global'))
+
+return
 
 const tableItems = []
 const cache = new yaml.Document({})
@@ -59,59 +68,92 @@ const cache = new yaml.Document({})
 let aliasDocName = ''
 let aliasSource = ''
 
-for (const [name, visitee] of docs) {
-	yaml.visit(visitee, {
-		Scalar(key, node, path) {
-			if (Identify.reference(node.value)) {
-				const refNode = new Reference(node)
+/** @param { Map<string, yaml.Document<any, any>> } docs */
+function createGetByReference(docs) {
+	/** @param { Reference } ref */
+	const getByReference = function _getByReference(ref) {
+		!cache.has(ref.path) && cache.set(ref.path, ref)
 
-				!cache.has(refNode.path) && cache.set(refNode.path, refNode)
+		if (ref.isRoot()) {
+			const [rootKey, ...paths] = ref.paths
 
-				if (refNode.isRoot()) {
-					const [rootKey, ...paths] = refNode.paths
-					!cache.has(rootKey) && cache.set(rootKey, docs.get(rootKey))
+			if (!docs.has(rootKey)) {
+				console.log(
+					u.red(
+						`The root key "${rootKey}" from "${ref.ref}" is not in the root`,
+					),
+				)
+			}
 
-					let currentNode = docs.get(rootKey)
+			let currentNode = docs.get(rootKey)
 
-					for (const path of paths) {
-						if (!currentNode || !path) break
-						if (currentNode.has(path)) {
-							if (yaml.isNode(currentNode.get(path))) {
-								currentNode = currentNode.get(path, true)
-								if (path === paths[paths.length - 1]) {
-									cache.setIn(refNode.paths, currentNode)
-								}
-							} else {
-								// console.log(
-								// 	`The next node after path "${path}" in "${refNode.paths}" is not a node!`,
-								// )
-							}
+			for (const path of paths) {
+				if (!currentNode || !path) break
+				if (yaml.isNode(currentNode)) {
+					if (currentNode.has(path)) {
+						if (!yaml.isNode(currentNode.get(path))) {
+							console.log(
+								u.red(
+									`The next node after path "${path}" in "${ref.ref}" is not a node`,
+								),
+							)
+						}
+						currentNode = currentNode.get(path, true)
+						if (path === paths[paths.length - 1]) {
+							// cache.setIn(refNode.paths, currentNode)
+							console.log(currentNode.toJSON())
 						}
 					}
-				} else if (refNode.isLocal()) {
-					const [localKey, ...paths] = refNode.paths
+				} else {
+					console.log(
+						u.red(
+							`currentNode is no longer a node. Current path: "${path}" from "${ref.ref}"`,
+						),
+					)
 				}
-
-				// if (!refs.has(refStr)) {
-				// 	refs.set(refStr, refNode)
-				// 	// let value = refNode.isLocal
-				// 	// 	? visitee.getIn(refNode.paths)
-				// 	// 	: docs.get(refNode.paths[0])?.getIn(refNode.paths[1].slice())
-				// 	Object.defineProperty(refNode, 'value', {
-				// 		configurable: true,
-				// 		enumerable: true,
-				// 		get: () =>
-				// 			refNode.isLocal
-				// 				? visitee.getIn(refNode.paths)
-				// 				: docs.get(refNode.paths[0])?.getIn(refNode.paths[1].slice()),
-				// 	})
-
-				// 	tableItems.push([refStr, refNode.value])
-				// }
 			}
-		},
-	})
+		} else if (ref.isLocal()) {
+			const [localKey, ...paths] = ref.paths
+		}
+	}
+
+	/** @param { (args: { getByReference: typeof getByReference, docs: Map<string, yaml.Document<any, any>}) => void} fn */
+	return (fn) => {
+		fn({
+			getByReference,
+		})
+	}
 }
+
+createGetByReference(docs)(({ getByReference }) => {
+	for (const [name, visitee] of docs) {
+		yaml.visit(visitee, {
+			Scalar(key, node, path) {
+				if (Identify.reference(node.value)) {
+					const refNode = new Reference(node)
+					const refValue = getByReference(refNode)
+
+					// if (!refs.has(refStr)) {
+					// 	refs.set(refStr, refNode)
+					// 	// let value = refNode.isLocal
+					// 	// 	? visitee.getIn(refNode.paths)
+					// 	// 	: docs.get(refNode.paths[0])?.getIn(refNode.paths[1].slice())
+					// 	Object.defineProperty(refNode, 'value', {
+					// 		configurable: true,
+					// 		enumerable: true,
+					// 		get: () =>
+					// 			refNode.isLocal
+					// 				? visitee.getIn(refNode.paths)
+					// 				: docs.get(refNode.paths[0])?.getIn(refNode.paths[1].slice()),
+					// 	})
+
+					// 	tableItems.push([refStr, refNode.value])
+					// }
+				}
+			},
+		})
+	}
+})
 
 // console.table(tableItems)
 // console.log(Array.from(docs.keys()))
