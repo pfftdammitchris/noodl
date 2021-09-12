31e0b31dceb77973319e27c1190ea5e7507ce0dc
Christopher Tran <pfftdammitchris@gmail.com>
2021-09-12 09:52:15 -0700

    G

--- scripts/downloadApp.js
+++ scripts/downloadApp.js
@@ -6,6 +6,7 @@ import yaml from 'yaml'
 import fs from 'fs-extra'
 
 const CONFIG_PATH = path.resolve(path.join(process.cwd(), 'config.yml'))
+const LOG_PATH = path.resolve(path.join(process.cwd(), 'logs/downloadApp.json'))
 const everyMinute = '* * * * *'
 const everyHour = '*/60 * * * *'
 const schedule = everyHour
@@ -27,7 +28,18 @@ if (!fs.existsSync(CONFIG_PATH)) {
 	u.log(`Created missing config file at ${u.yellow(CONFIG_PATH)}`)
 }
 
+if (!fs.existsSync(LOG_PATH)) {
+	fs.writeJsonSync(LOG_PATH, {})
+	u.log(`Created missing log file at ${u.yellow(LOG_PATH)}`)
+}
+
 const config = yaml.parseDocument(fs.readFileSync(CONFIG_PATH, 'utf8'))
+const logFile = fs.readJsonSync(LOG_PATH)
+
+if (!('executions' in logFile)) {
+	logFile.executions = []
+	fs.writeJsonSync(LOG_PATH, logFile, { spaces: 2 })
+}
 
 if (!config.has('apps')) {
 	const initialValue = new yaml.YAMLSeq()
@@ -38,12 +50,15 @@ if (!config.has('apps')) {
 
 const apps = config.get('apps')?.toJSON?.() || []
 
-console.log({ apps })
+console.log({ apps, previousExecutions: logFile.executions?.length || 0 })
 
 cron.schedule(
 	schedule,
 	async () => {
 		try {
+			const success = []
+			const failed = []
+
 			for (const appName of apps) {
 				try {
 					await execa.command(
@@ -53,9 +68,20 @@ cron.schedule(
 							stdio: 'inherit',
 						},
 					)
+					success.push(appName)
 				} catch (error) {
 					console.error(error)
+					failed.push(appName)
 				}
+
+				if (!u.isArr(logFile.executions)) logFile.executions = []
+
+				logFile.executions.push({
+					timestamp: Date.now(),
+					apps,
+					success,
+					failed,
+				})
 			}
 		} catch (error) {
 			console.error(error)
